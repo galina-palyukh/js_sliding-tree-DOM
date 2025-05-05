77b123c84b1695f857cfb13b808b32611e0a3799
g.palyukh <g.palyukh@gmail.com>
2025-05-05 21:28:27 +0200

    check insideUl before accessing

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -15,7 +15,7 @@ document.addEventListener('click', (e) => {
   if (e.target.tagName === 'SPAN') {
     const insideUl = e.target.nextElementSibling;
 
-    if (insideUl && insideUl.style.display === 'none') {
+    if (insideUl.tagName === 'UL' && insideUl.style.display === 'none') {
       insideUl.style.display = 'block';
     } else {
       insideUl.style.display = 'none';
